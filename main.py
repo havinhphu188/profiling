654ae58eeb4ac46189d96f07f3539d6fdbc36dd9
Ha Vinh Phu <havinhphu188@gmail.com>
2022-07-24 17:56:50 +0700

    change

--- main.py
+++ main.py
@@ -1,66 +1,19 @@
-import requests
-import re
-import time
+from sqlalchemy import create_engine
+engine = create_engine(
+    "postgresql+pg8000://postgres:asd123@localhost:5432/sqlalchemy-learn",
+    echo=True
+)
 
-import httpx
-import asyncio
+from sqlalchemy import text
 
+with engine.connect() as conn:
+    result = conn.execute(text("select 'hello world'"))
+    print(result.all())
 
-def count_https_in_web_pages():
-    with open('top15USWebsites.txt', 'r', encoding='utf-8') as f:
-        urls = [line.strip() for line in f.readlines()]
-
-    htmls = []
-    for url in urls:
-        htmls = htmls + [requests.get(url).text]
-
-    count_https = 0
-    count_http = 0
-    for html in htmls:
-        count_https += len(re.findall("https://", html))
-        count_http += len(re.findall("http://", html))
-
-    print('finished parsing')
-    time.sleep(2.0)
-    print(f'{count_https=}')
-    print(f'{count_http=}')
-    print(f'{count_https/count_http=}')
-
-
-async def better_count_https_in_web_pages():
-    with open('top15USWebsites.txt', 'r', encoding='utf-8') as f:
-        urls = [line.strip() for line in f.readlines()]
-
-    async with httpx.AsyncClient() as client:
-        tasks = (client.get(url, follow_redirects=True) for url in urls)
-        reqs = await asyncio.gather(*tasks)
-
-    htmls = [req.text for req in reqs]
-
-    count_https = 0
-    count_http = 0
-    for html in htmls:
-        count_https += len(re.findall("https://", html))
-        count_http += len(re.findall("http://", html))
-
-    print('finished parsing')
-    print(f'{count_https=}')
-    print(f'{count_http=}')
-    print(f'{count_https/count_http=}')
-
-
-def main():
-    import cProfile
-    import pstats
-
-    with cProfile.Profile() as pr:
-        asyncio.run(better_count_https_in_web_pages())
-
-    stats = pstats.Stats(pr)
-    stats.sort_stats(pstats.SortKey.TIME)
-    # stats.print_stats()
-    stats.dump_stats(filename='needs_profiling.prof')
-
-
-if __name__ == '__main__':
-    main()
+with engine.connect() as conn:
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}]
+    )
+    conn.commit()
