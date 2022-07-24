ea5a1cfb88aff680da082d74e7b8582ed83c1ae7
Ha Vinh Phu <havinhphu188@gmail.com>
2022-07-24 23:27:43 +0700

    change

--- main.py
+++ main.py
@@ -47,6 +47,28 @@ session.add_all([
 ed_user.nickname = 'eddie'
 # print(session.dirty)
 # print(session.new)
+
 session.commit()
 
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+class Address(Base):
+    __tablename__ = 'addresses'
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="addresses")
+    def __repr__(self):
+        return "<Address(email_address='%s')>" % self.email_address
+
+User.addresses = relationship(
+    "Address", order_by=Address.id, back_populates="user")
+
+# Base.metadata.create_all(engine)
+jack = User(name='jack', fullname='Jack Bean', nickname='gjffdd')
+# print(jack.addresses)
+jack.addresses = [
+                Address(email_address='jack@google.com'),
+                Address(email_address='j25@yahoo.com')]
