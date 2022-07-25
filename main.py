780d8281bac7c820e6e06f8b932d8161e6121827
Ha Vinh Phu <phu.hv@teko.vn>
2022-07-25 09:06:54 +0700

    runnable sql

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 engine = create_engine(
-    "postgresql+pg8000://postgres:asd123@localhost:5432/sqlalchemy-learn",
+    "mysql+pymysql://root:123456@localhost:3306/sql_alchemy",
     echo=True
 )
 
@@ -12,63 +12,63 @@ from sqlalchemy import Column, Integer, String
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
+    name = Column(String(50))
+    fullname = Column(String(50))
+    nickname = Column(String(50))
     def __repr__(self):
        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
                             self.name, self.fullname, self.nickname)
        
 print (User.__table__ )
 
-# Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 
+# # ed_user = User(name='ed', fullname='Ed Jones', nickname='edsnickname')
+# # print(ed_user.name)
+# # print(ed_user.nickname)
+# # print(str(ed_user.id))
+
+# from sqlalchemy.orm import sessionmaker
+# Session = sessionmaker(bind=engine)
+# session = Session()
 # ed_user = User(name='ed', fullname='Ed Jones', nickname='edsnickname')
-# print(ed_user.name)
-# print(ed_user.nickname)
-# print(str(ed_user.id))
+# session.add(ed_user)
 
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-session = Session()
-ed_user = User(name='ed', fullname='Ed Jones', nickname='edsnickname')
-session.add(ed_user)
+# our_user = session.query(User).filter_by(name='ed').first() 
 
-our_user = session.query(User).filter_by(name='ed').first() 
+# # print(our_user)
+# # print(ed_user is our_user)
 
-# print(our_user)
-# print(ed_user is our_user)
+# session.add_all([
+#     User(name='wendy', fullname='Wendy Williams', nickname='windy'),
+#     User(name='mary', fullname='Mary Contrary', nickname='mary'),
+#     User(name='fred', fullname='Fred Flintstone', nickname='freddy')])
 
-session.add_all([
-    User(name='wendy', fullname='Wendy Williams', nickname='windy'),
-    User(name='mary', fullname='Mary Contrary', nickname='mary'),
-    User(name='fred', fullname='Fred Flintstone', nickname='freddy')])
+# ed_user.nickname = 'eddie'
+# # print(session.dirty)
+# # print(session.new)
 
-ed_user.nickname = 'eddie'
-# print(session.dirty)
-# print(session.new)
-
-session.commit()
+# session.commit()
 
 
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
+# from sqlalchemy import ForeignKey
+# from sqlalchemy.orm import relationship
 
-class Address(Base):
-    __tablename__ = 'addresses'
-    id = Column(Integer, primary_key=True)
-    email_address = Column(String, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship("User", back_populates="addresses")
-    def __repr__(self):
-        return "<Address(email_address='%s')>" % self.email_address
+# class Address(Base):
+#     __tablename__ = 'addresses'
+#     id = Column(Integer, primary_key=True)
+#     email_address = Column(String, nullable=False)
+#     user_id = Column(Integer, ForeignKey('users.id'))
+#     user = relationship("User", back_populates="addresses")
+#     def __repr__(self):
+#         return "<Address(email_address='%s')>" % self.email_address
 
-User.addresses = relationship(
-    "Address", order_by=Address.id, back_populates="user")
+# User.addresses = relationship(
+#     "Address", order_by=Address.id, back_populates="user")
 
-# Base.metadata.create_all(engine)
-jack = User(name='jack', fullname='Jack Bean', nickname='gjffdd')
-# print(jack.addresses)
-jack.addresses = [
-                Address(email_address='jack@google.com'),
-                Address(email_address='j25@yahoo.com')]
+# # Base.metadata.create_all(engine)
+# jack = User(name='jack', fullname='Jack Bean', nickname='gjffdd')
+# # print(jack.addresses)
+# jack.addresses = [
+#                 Address(email_address='jack@google.com'),
+#                 Address(email_address='j25@yahoo.com')]
